f979aee3d8d6cd59868cad3245b52d2255002ba7
Solomon Garber <solomongarber@Solomons-MacBook-Air.local>
2015-11-17 15:13:34 -0500

    no disk

--- dataQueue.py
+++ dataQueue.py
@@ -12,16 +12,17 @@ class dataQueue:
         for i in range(nD+1):
             self.frames.append(np.zeros(size,dtype=np.uint8))
         self.now=np.array(self.frames[self.middle],dtype=np.int)
+        self.used=deque()
             
 
 
     def add_frame(self, frame):
         self.frames.append(frame)
-        self.frames.popleft()
+        self.used.append(self.frames.popleft())
         self.now=self.frames[self.middle]
 
-    def add_frame_left(self,frame):
-        self.frames.appendleft(frame)
+    def add_frame_left(self):
+        self.frames.appendleft(self.used.pop())
         self.frames.pop()
         self.now=self.frames[self.middle]
 
@@ -29,12 +30,12 @@ class dataQueue:
         return self.frames[label]
 
     def get_dcost(self,label):
-        return .1* np.sum(np.abs(np.array(self.now,dtype=np.int)-np.array(self.frames[label+1]),dtype=np.int),1)
+        return 10*np.sum(np.abs(np.array(self.now,dtype=np.int)-np.array(self.frames[label+1],dtype=np.int),dtype=np.int),1)
 
     def get_scost(self,frame):
         smenergy=np.zeros((self.num_pixels,self.nD),dtype=np.int)
         for l in range(self.nD):
-            smenergy[:,l]=np.sum(np.power(np.abs(np.array(self.frames[l],dtype=np.int)-np.array(frame,dtype=np.int)),3),1)
+            smenergy[:,l]=np.sum(np.power(np.abs(np.array(self.frames[l],dtype=np.int)-np.array(frame,dtype=np.int)),2),1)
         return smenergy
 
     def get_output(self,labels):
@@ -43,7 +44,7 @@ class dataQueue:
         for l in range(self.nD):
             bad_idea[:,:,l]=self.get_frame(l+1)
         for color in range(3):
-            output[:,:,color]=np.reshape(bad_idea[range(self.num_pixels),color,self.nD-labels-1],(self.frame_shape[1],self.frame_shape[0]))
+            output[:,:,color]=np.reshape(bad_idea[range(self.num_pixels),color,labels],(self.frame_shape[1],self.frame_shape[0]))
             #output[:,:,color]=np.reshape(10*labels,(self.frame_shape[1],self.frame_shape[0]))
         return output
 
